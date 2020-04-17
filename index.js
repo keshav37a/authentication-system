bba60411ace150c6819ab18bc31ce7b405d406f7
Keshav Mathur <keshav37a@gmail.com>
2020-04-17 10:20:54 +0530

    set up basic folder structure

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const port = 8000;
+const path = require('path');
 const express = require('express');
-
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', function(req, res){
+    res.render('home');
+})
+
 app.listen(port, function(err){
     if(err){console.log(`error in running app on local-host- ${err}`);}
 
