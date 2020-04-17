853522233034f842baf71f2953a5fc8471b46ba7
Keshav Mathur <keshav37a@gmail.com>
2020-04-17 11:04:55 +0530

    add routes and views for login and signup pages

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,3 +3,12 @@ module.exports.home = async function(req, res){
     return res.render('home.ejs');
 }
 
+module.exports.signIn = async function(req, res){
+    console.log('Inside home_controller.signIn');
+    return res.render('sign-in.ejs');
+}
+
+module.exports.signUp = async function(req, res){
+    console.log('Inside home_controller.signUp');
+    return res.render('sign-up.ejs');
+}
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
 router.get('/', homeController.home);
+router.get('/sign-in', homeController.signIn);
+router.get('/sign-up', homeController.signUp);
 
 module.exports = router;
